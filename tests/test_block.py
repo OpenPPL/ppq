345fb0c123f49c1ac99a98b0369c6cae3d0bf2b0
jizhe <jizhe1@sensetime.com>
2022-04-18 09:23:36 +0000

    modify ci test

--- tests/test_block.py
+++ tests/test_block.py
@@ -2,6 +2,7 @@ from tmodel  import *
 from tscheme import *
 from ppq     import *
 from ppq.api import *
+import sys
 
 DEVICE = 'cuda'
 
@@ -15,12 +16,12 @@ for scheme in TEST_SCHEMES:
             reference_outputs = torch.cat([model(batch) for batch in dataset])
 
             quantized = quantize_torch_model(
-                model=model, 
-                calib_dataloader=dataset, 
-                calib_steps=8, 
+                model=model,
+                calib_dataloader=dataset,
+                calib_steps=8,
                 input_shape=case.input_generator().shape,
                 platform=scheme.quant_platform,
-                setting=scheme.setting, 
+                setting=scheme.setting,
                 verbose=False)
 
             executor = TorchExecutor(quantized)
@@ -41,9 +42,10 @@ for scheme in TEST_SCHEMES:
             if (case.deploy_platforms is None or
                 scheme.export_platform in case.deploy_platforms):
                 export_ppq_graph(
-                    graph=quantized, 
-                    platform=scheme.export_platform, 
+                    graph=quantized,
+                    platform=scheme.export_platform,
                     graph_save_to='tworkingspace/export',
                     config_save_to='tworkingspace/export.json')
         except NotImplementedError as e:
-            pass
+            print(f'{time.strftime("%Y-%m-%d %H:%M:%S")} | Error occured: {e}')
+            sys.exit(1)
