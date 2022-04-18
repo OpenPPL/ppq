c82c46c28dff9a9d1d3993ba9323cfa5b026532c
Jzz <41146729+Jzz24@users.noreply.github.com>
2022-04-18 17:39:33 +0800

    modify ci test (#62)
    modify ci test

--- tests/test_system.py
+++ tests/test_system.py
@@ -2,27 +2,33 @@ from tmodel  import *
 from tscheme import *
 from ppq     import *
 from ppq.api import *
+import sys
 
 DEVICE = 'cuda'
 
 for scheme in TEST_SCHEMES:
     for case in TORCH_TEST_CASES:
-        print(f'PPQ System test start with model {case.model_name}, Scheme: {scheme.name}')
-        dataset = [case.input_generator().to(DEVICE) for _ in range(8)]
-        model = case.model_builder().to(DEVICE)
+        try:
+            print(f'PPQ System test start with model {case.model_name}, Scheme: {scheme.name}')
+            dataset = [case.input_generator().to(DEVICE) for _ in range(8)]
+            model = case.model_builder().to(DEVICE)
 
-        quantized = quantize_torch_model(
-            model=model, 
-            calib_dataloader=dataset, 
-            calib_steps=8, 
-            input_shape=case.input_generator().shape,
-            platform=scheme.quant_platform,
-            setting=scheme.setting)
+            quantized = quantize_torch_model(
+                model=model,
+                calib_dataloader=dataset,
+                calib_steps=8,
+                input_shape=case.input_generator().shape,
+                platform=scheme.quant_platform,
+                setting=scheme.setting)
 
-        if (case.deploy_platforms is None or 
-            scheme.export_platform in case.deploy_platforms):
-            export_ppq_graph(
-                graph=quantized, 
-                platform=scheme.export_platform, 
-                graph_save_to='tworkingspace/export',
-                config_save_to='tworkingspace/export.json')
+            if (case.deploy_platforms is None or
+                scheme.export_platform in case.deploy_platforms):
+                export_ppq_graph(
+                    graph=quantized,
+                    platform=scheme.export_platform,
+                    graph_save_to='tworkingspace/export',
+                    config_save_to='tworkingspace/export.json')
+        except NotImplementedError as e:
+            print(f'{time.strftime("%Y-%m-%d %H:%M:%S")} | Error occured: {e}')
+            sys.exit(1)
+
