1630a930f29f38c61519b1f0295b77e0524c48b5
AwesomeCodingBoy <43309460+ZhangZhiPku@users.noreply.github.com>
2022-05-10 22:26:28 +0800

    修正了一个波吉脑袋歪了的问题

--- ppq/core/__init__.py
+++ ppq/core/__init__.py
@@ -1,45 +1,44 @@
-"""...::iirii:.. P7Ysi.  iS7u.
-
-..:::irri:rPBBBBBBBBBBBBBBBBBBBBBBBg57. B   :Js:B7 .PS
-:sEBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBDv B      .:     u
-:5BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB2 DQ
-SBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBQBBBBBBBBBBBBBBBBBBBBBBBBBBRi JZ  ..i:.
-.dBBBBBBBBBBBBBBBBBBBBBBBg7          .BQBBBBBBBBBBBBBBBBBBBBBBBBBBBBBD7 YQ.
-vBBBBBBBBBBBBBBBBBBBBBBMY.              QBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB7 2D
-YBBBBBBBBBBBBBBBBBBBBZ7                   BBBBBBBBBBBBBBr
-SBBBBBBBBBBBBBBBBBBBBM:
-
-               :Bi  UBBBBBBBBBBBBBBBBBPv.                       BBBBBBBBBBBBj    .BBBBBBBBBBBBBBBBBBBBBBY
-                 ZQBBBBBBBBBBBBBBE                             DBBBBBBBBBBS        .PBBBBBBBBBBBBBBBBBBBBBu
-                ZBBBBBBBBBBBBBBBZ                             EBBBBBBBBb:             :PBBBBBBBBBBBBBBBBBBBB
-             .iBBBBBBBBBBBBBBBBB          ..:i:.            .BBBBBQKr                     7gBBBBBBBBBBBBBBBBB
-             gBBBBBBBBBBBBBBBBQ      .irri::...           IR2r.                   rir7j17.    :LBBBBQRQBBBBBBB
-             BBBBBBBBBBBBBBBB:     rur.                 .i.                              iKg7     vBBBBBBBBBBB:
-            BBBBBBBBBBBBBBP.      Xr    ..:r7rrr.                              .::LKIvrr.   5Br     :BBBBBBBBB1
-           bBBBBBBBBBBZ7.            :Yr.     :dBBP                         .JYi.      :PBE   E       BBBBBBBBr
-          jBBBBBBBBBB              :Q7    .rv:   vBb                        Qi            BB:         :BBBBBBB.
-          BBBBBBBBBBr              B.   BBBBBBBBQ                                5BBBBBQi  dB          IBBBBQB5
-         :BBBBBBBBBB                   JBBBBBBBBB:                              BBBBBBBBB:               rBBBBB
-          BBBBBBBBB1                    XBBBBBBU                                ZBBBBBBBB.                EBBBB7MQ
-          BBBBBBBBB7                                                              .LU1r.                  iBBBb  JBv
-          BBBBBBBBg                  5D7                         ..                        .              :BBB7   rB
-          qB: gBBBK           i  : .  .Y21JJL7i.                 ..B:             ULi::iirr:..            .BBu  :  B7
-        gd Ui  QBBE            .   :.                              r.              .....    :i. :.        PBR     .B.
-       7B .    iBBB                                                                             .        .B 7r    BL
-       BB .     UBBv                                                                                     BP .   rBr
-       MB      q  .                                                                                     XS    rDj:
-       .BB    .::. :                  iLv7ri:.                                      ...                ..  :YUr
-         BB7     .:U                   ...irvUXPP27:                      ..:71SqP2Is7i              Ui::rr:
-          .BBg:                                  .rLr:r7i..        .:irrrii:..                      Bg
-             7QBE7.                                     .:r77v .rri:.                             rB7
-                :JdDE277S.                                   ..r                                rDK
-                      . rBBi                                                                .is5r
-                          :gBEr                                                          :7jL:
-                             :SQMSr.                                                 .:7r:.
-                                 :YKPbur::..                                 ...irrqBq.
-                                      :KgBQU1UUJr:          i7777vsv777v777ri:...    rdQJ.
-                                    7qj:                          ..              .     7QBr
-                                  rP:                                                      bB
+"""
+                                                                                                     
+                                                              ...::iirii:..                                             
+                P7Ysi.  iS7u.                ..:::irri:rPBBBBBBBBBBBBBBBBBBBBBBBg57.                                    
+                B   :Js:B7 .PS        :sEBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBDv                                
+                B      .:     u  :5BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB2                             
+                DQ            SBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBQBBBBBBBBBBBBBBBBBBBBBBBBBBRi                          
+           JZ  ..i:.      .dBBBBBBBBBBBBBBBBBBBBBBBg7          .BQBBBBBBBBBBBBBBBBBBBBBBBBBBBBBD7                       
+            YQ.         vBBBBBBBBBBBBBBBBBBBBBBMY.              QBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB7                    
+              2D      YBBBBBBBBBBBBBBBBBBBBZ7                   BBBBBBBBBBBBBBr SBBBBBBBBBBBBBBBBBBBBM:                 
+               :Bi  UBBBBBBBBBBBBBBBBBPv.                       BBBBBBBBBBBBj    .BBBBBBBBBBBBBBBBBBBBBBY               
+                 ZQBBBBBBBBBBBBBBE                             DBBBBBBBBBBS        .PBBBBBBBBBBBBBBBBBBBBBu             
+                ZBBBBBBBBBBBBBBBZ                             EBBBBBBBBb:             :PBBBBBBBBBBBBBBBBBBBB            
+             .iBBBBBBBBBBBBBBBBB          ..:i:.            .BBBBBQKr                     7gBBBBBBBBBBBBBBBBB           
+             gBBBBBBBBBBBBBBBBQ      .irri::...           IR2r.                   rir7j17.    :LBBBBQRQBBBBBBB          
+             BBBBBBBBBBBBBBBB:     rur.                 .i.                              iKg7     vBBBBBBBBBBB:         
+            BBBBBBBBBBBBBBP.      Xr    ..:r7rrr.                              .::LKIvrr.   5Br     :BBBBBBBBB1         
+           bBBBBBBBBBBZ7.            :Yr.     :dBBP                         .JYi.      :PBE   E       BBBBBBBBr        
+          jBBBBBBBBBB              :Q7    .rv:   vBb                        Qi            BB:         :BBBBBBB.        
+          BBBBBBBBBBr              B.   BBBBBBBBQ                                5BBBBBQi  dB          IBBBBQB5        
+         :BBBBBBBBBB                   JBBBBBBBBB:                              BBBBBBBBB:               rBBBBB        
+          BBBBBBBBB1                    XBBBBBBU                                ZBBBBBBBB.                EBBBB7MQ     
+          BBBBBBBBB7                                                              .LU1r.                  iBBBb  JBv   
+          BBBBBBBBg                  5D7                         ..                        .              :BBB7   rB    
+          qB: gBBBK           i  : .  .Y21JJL7i.                 ..B:             ULi::iirr:..            .BBu  :  B7  
+        gd Ui  QBBE            .   :.                              r.              .....    :i. :.        PBR     .B.  
+       7B .    iBBB                                                                             .        .B 7r    BL   
+       BB .     UBBv                                                                                     BP .   rBr    
+       MB      q  .                                                                                     XS    rDj:      
+       .BB    .::. :                  iLv7ri:.                                      ...                ..  :YUr         
+         BB7     .:U                   ...irvUXPP27:                      ..:71SqP2Is7i              Ui::rr:          
+          .BBg:                                  .rLr:r7i..        .:irrrii:..                      Bg                
+             7QBE7.                                     .:r77v .rri:.                             rB7                 
+                :JdDE277S.                                   ..r                                rDK                   
+                      . rBBi                                                                .is5r                     
+                          :gBEr                                                          :7jL:                        
+                             :SQMSr.                                                 .:7r:.                            
+                                 :YKPbur::..                                 ...irrqBq.                                 
+                                      :KgBQU1UUJr:          i7777vsv777v777ri:...    rdQJ.                              
+                                    7qj:                          ..              .     7QBr                            
+                                  rP:                                                      bB            
 
 Sensetime PPL Quant Tool(PPQ) is a neural network quantization tool for high-performance deep learning inference.
 It includes necessary network parsers, quant-simulator and optimization algorithms.
