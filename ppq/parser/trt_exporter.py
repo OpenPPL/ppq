60b6cadcb4a746869627dbefcc3feca11446d2d1
Lenan22 <cugb_wulenan@163.com>
2022-10-21 15:34:35 +0800

    feat/20221021-add-wts-export

--- ppq/parser/trt_exporter.py
+++ ppq/parser/trt_exporter.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import struct
 from typing import List
 from ppq.core import (DataType, PPQ_CONFIG, NetworkFramework, QuantizationProperty,
                       QuantizationStates)
@@ -10,6 +11,7 @@ from .caffe_exporter import CaffeExporter
 from .onnx_exporter import OnnxExporter
 from .util import convert_value
 from ppq.core import ppq_warning
+from ppq.core import ppq_info
 
 class TensorrtExporter(GraphExporter):
     def export_quantization_config(self, config_path: str, graph: BaseGraph):
@@ -48,11 +50,36 @@ class TensorrtExporter(GraphExporter):
         with open(config_path, "w") as json_file:
             json_file.write(json_qparams_str)
 
+    def export_weights(self, graph: BaseGraph, config_path: str = None):
+        topo_order =  graph.topological_sort()
+        weights_list = []
+        for index, op in enumerate(topo_order):
+            if op.type in {"Conv", "Gemm"}:
+                weights_list.extend(op.parameters)
+
+        weight_file_path = os.path.join(os.path.dirname(config_path), "quantized.wts")
+
+        f = open(weight_file_path, 'w')
+        f.write("{}\n".format(len(weights_list)))
+
+        for param in weights_list:
+            weight_name = param.name
+            weight_value = param.value.reshape(-1).cpu().numpy()
+            f.write("{} {}".format(weight_name, len(weight_value)))
+            for value in weight_value:
+                f.write(" ")
+                f.write(struct.pack(">f", float(value)).hex())
+            f.write("\n")
+        ppq_info(f'Parameters have been saved to file: {weight_file_path}')
+
 
     def export(self, file_path: str, graph: BaseGraph, config_path: str = None, input_shapes: List[List[int]] = [[1, 3, 224, 224]]):
         if not PPQ_CONFIG.EXPORT_DEVICE_SWITCHER:
             processor = GraphDeviceSwitcher(graph)
             processor.remove_switcher()
+
+        self.export_weights(graph, config_path)
+
         if config_path is not None:
             self.export_quantization_config(config_path, graph)
 
